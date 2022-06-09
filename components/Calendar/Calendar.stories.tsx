dd41210a849ad8c09b2cdaa5c7468f6cd10a9da7
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2022-06-09 16:43:38 +0700

    fix: fix calendar

--- components/Calendar/Calendar.stories.tsx
+++ components/Calendar/Calendar.stories.tsx
@@ -2,9 +2,5 @@ import { storiesOf } from '@storybook/react'
 import { Calendar } from './Calendar'
 
 storiesOf('components/Calendar', module).add('basic', () => {
-  return (
-    <div>
-      <Calendar year={2022} month={6} today={9} hasEvent={[5, 26]} />
-    </div>
-  )
+  return <Calendar year={2022} month={6} today={9} hasEvent={[5, 26]} />
 })
--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { IconAngleLeftSolid } from 'components/icons/components/IconAngleLeftSolid'
 import { IconAngleRightSolid } from 'components/icons/components/IconAngleRightSolid'
 import cx from 'classnames'
@@ -27,88 +28,87 @@ const months = [
 export function Calendar(props: CalendarProps) {
   const { year, month, today, hasEvent } = props
 
-  /* Calculate start day of current month (2 -> 8) */
-  const d = new Date(`${months[month - 1]}, ${year}`)
-  const startDay = d.getDay() === 0 ? 8 : d.getDay() + 1
+  const renderDayList = useMemo(() => {
+    /* Calculate start day of current month (2 -> 8) */
+    const d = new Date(`${months[month - 1]}, ${year}`)
+    const startDay = d.getDay() === 0 ? 8 : d.getDay() + 1
 
-  const prevMonth = month - 1 === 0 ? 12 : month - 1
-  const countPrevMonth =
-    prevMonth === 12
-      ? new Date(year - 1, prevMonth, 0).getDate()
-      : new Date(year - 1, prevMonth, 0).getDate()
+    const prevMonth = month - 1 === 0 ? 12 : month - 1
+    const countPrevMonth =
+      prevMonth === 12
+        ? new Date(year - 1, prevMonth, 0).getDate()
+        : new Date(year - 1, prevMonth, 0).getDate()
 
-  /* 3 parts displayed on the calendar */
-  const daysOfPrevMonth = startDay - 2
-  const daysOfCurrMonth = new Date(year, month, 0).getDate()
-  const daysOfNextMonth =
-    7 * Math.ceil((daysOfPrevMonth + daysOfCurrMonth) / 7) -
-    daysOfPrevMonth -
-    daysOfCurrMonth
+    /* 3 parts displayed on the calendar */
+    const daysOfPrevMonth = startDay - 2
+    const daysOfCurrMonth = new Date(year, month, 0).getDate()
+    const daysOfNextMonth =
+      7 * Math.ceil((daysOfPrevMonth + daysOfCurrMonth) / 7) -
+      daysOfPrevMonth -
+      daysOfCurrMonth
 
-  const daysOnCalendar = daysOfPrevMonth + daysOfCurrMonth + daysOfNextMonth
+    const daysOnCalendar = daysOfPrevMonth + daysOfCurrMonth + daysOfNextMonth
 
-  const dayList = []
+    const dayList = []
 
-  for (let i = 0; i < daysOnCalendar; i++) {
-    /* Current month */
-    if (i >= daysOfPrevMonth && i < daysOfPrevMonth + daysOfCurrMonth) {
-      dayList.push(
-        <button className="py-2 bg-white">
-          <div
-            className={cx('py-1', {
-              'bg-gray-900 rounded-full text-white font-medium w-8 aspect-square mx-auto':
-                i + 1 - daysOfPrevMonth === today,
-              'text-indigo-600 font-medium': hasEvent.includes(
-                i + 1 - daysOfPrevMonth,
-              ),
-            })}
-          >
-            {i - daysOfPrevMonth + 1}
-          </div>
-        </button>,
-      )
-    } /* Days of previous or next month */ else
-      dayList.push(
-        <button className="py-2 bg-gray-100" disabled>
-          <div className="py-1 text-gray-400">
-            {i < daysOfPrevMonth
-              ? countPrevMonth - daysOfPrevMonth + i + 1
-              : i - daysOfPrevMonth - daysOfCurrMonth + 1}
-          </div>
-        </button>,
-      )
-  }
+    for (let i = 0; i < daysOnCalendar; i++) {
+      /* Current month */
+      if (i >= daysOfPrevMonth && i < daysOfPrevMonth + daysOfCurrMonth) {
+        dayList.push(
+          <button className="py-2 bg-white">
+            <div
+              className={cx('py-1', {
+                'bg-gray-900 rounded-full text-white font-medium w-8 aspect-square mx-auto':
+                  i + 1 - daysOfPrevMonth === today,
+                'text-indigo-600 font-medium': hasEvent.includes(
+                  i + 1 - daysOfPrevMonth,
+                ),
+              })}
+            >
+              {i - daysOfPrevMonth + 1}
+            </div>
+          </button>,
+        )
+      } /* Days of previous or next month */ else
+        dayList.push(
+          <button className="py-2 bg-gray-100" disabled>
+            <div className="py-1 text-gray-400">
+              {i < daysOfPrevMonth
+                ? countPrevMonth - daysOfPrevMonth + i + 1
+                : i - daysOfPrevMonth - daysOfCurrMonth + 1}
+            </div>
+          </button>,
+        )
+    }
+    return dayList
+  }, [year, month, today, hasEvent])
 
   return (
-    <div>
-      <div className="max-w-sm">
-        <div className="flex justify-between mb-7">
-          <button>
-            <IconAngleLeftSolid className="h-5 w-5 fill-gray-400" />
-          </button>
-          <button>
-            <h3 className="text-xl font-semibold">
-              {months[(month - 1) % 12]}
-            </h3>
-          </button>
-          <button>
-            <IconAngleRightSolid className="h-5 w-5 fill-gray-400" />
-          </button>
-        </div>
+    <div className="max-w-sm">
+      <div className="flex justify-between mb-7">
+        <button>
+          <IconAngleLeftSolid className="h-5 w-5 fill-gray-400" />
+        </button>
+        <button className="text-xl font-semibold">
+          {months[(month - 1) % 12]}
+        </button>
+        <button>
+          <IconAngleRightSolid className="h-5 w-5 fill-gray-400" />
+        </button>
+      </div>
 
-        <div className="grid grid-cols-7 gap-0 text-center text-gray-500 mb-3">
-          <span>M</span>
-          <span>T</span>
-          <span>W</span>
-          <span>T</span>
-          <span>F</span>
-          <span>S</span>
-          <span>S</span>
-        </div>
+      <div className="grid grid-cols-7 gap-0 text-center text-gray-500 mb-3">
+        <span>M</span>
+        <span>T</span>
+        <span>W</span>
+        <span>T</span>
+        <span>F</span>
+        <span>S</span>
+        <span>S</span>
+      </div>
 
-        <div className="shadow-sm grid grid-cols-7 gap-px border border-gray-200 bg-gray-200 rounded-xl overflow-hidden">
-          {dayList}
-        </div>
+      <div className="shadow-sm grid grid-cols-7 gap-px border border-gray-200 bg-gray-200 rounded-xl overflow-hidden">
+        {renderDayList}
       </div>
     </div>
   )
